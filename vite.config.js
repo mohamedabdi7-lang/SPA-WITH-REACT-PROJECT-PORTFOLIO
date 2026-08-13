4efbe445bd85d8edaadae092424c3dd60b277170
mohamedabdi7-lang <mohamedsiraj2025k@gmail.com>
2026-08-13 09:16:43 +0300

    fix: set relative base path for production assets in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/SPA_with_react_project_portfolio/',
+  base: './SPA_with_react_project_portfolio/',
 })
